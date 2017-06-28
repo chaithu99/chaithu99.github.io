f1abaa5360f54144821371db6c9d7b8e90e4dd21
chaithu99 <chaithanya25399@gmail.com>
2017-06-28 12:42:31 +0530

    added basic files

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $('.hover-test').hover(
+    function(){
+    $(this).addClass('active');
+    },
+    function(){
+     $(this).removeClass('active');   
+    }); });
+ 
+
+
